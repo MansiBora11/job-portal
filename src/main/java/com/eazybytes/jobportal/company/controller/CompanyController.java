528a6caf805f7b5fc8d3ce890359abb3caa33073
MansiBora11 <boramansi118@gmail.com>
2026-07-11 08:18:26 +0530

    Add changes for dto

--- src/main/java/com/eazybytes/jobportal/company/controller/CompanyController.java
+++ src/main/java/com/eazybytes/jobportal/company/controller/CompanyController.java
@@ -1,15 +1,29 @@
 package com.eazybytes.jobportal.company.controller;
 
+import com.eazybytes.jobportal.dto.CompanyDto;
+import com.eazybytes.jobportal.entity.Company;
+import com.eazybytes.jobportal.service.ICompanyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/companies")
+@RequiredArgsConstructor
 public class CompanyController {
+
+    private final ICompanyService companyService;
+
+
     @GetMapping(version = "1.0")
-    public ResponseEntity<String> getAllCompanies() {
-        return ResponseEntity.ok().body("Companies List");
+    public ResponseEntity<List<CompanyDto>> getAllCompanies() {
+        List<CompanyDto> companies = companyService.getAllCompanies();
+        return ResponseEntity.ok().body(companies);
     }
+
+
 }
